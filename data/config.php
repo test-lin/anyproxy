04395c73d4d6512508c1c29d4d4781e1df649e36
jani <505932384@qq.com>
2018-04-04 20:03:17 +0800

    ???

---
+++ data/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'db_type' => 'mysqli',
+    'mysqli' => [
+        'host' => '127.0.0.1',
+        'prot' => 3306,
+        'username' => 'root',
+        'password' => 'root',
+        'dbname' => 'anyproxy'
+    ]
+];
