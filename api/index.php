5f02274efb4bc0b0718dfe5d24482961513ede9d
jani <505932384@qq.com>
2018-05-08 14:19:37 +0800

    更新

--- api/index.php
+++ api/index.php
@@ -19,12 +19,16 @@ class Tool
     public function getMsgJson()
     {
         $contnet = trim($_POST['str']);
+        $nickname = $_POST['nickname'] ?? '';
+        $nickname = trim($nickname);
+        $logo = $_POST['logo'] ?? '';
+        $logo = trim($logo);
 
         //先针对url参数进行操作
         parse_str(parse_url(htmlspecialchars_decode(urldecode($_POST['url'])), PHP_URL_QUERY), $query); //解析url地址
         $biz = $query['__biz'] ?? ''; //得到公众号的biz
         //接下来进行以下操作
-        $this->checkBiz($biz);
+        $user_id = $this->checkBiz($biz, $nickname, $logo);
 
         // 文章列表
         $json = json_decode($contnet, true); //首先进行json_decode
@@ -49,7 +53,7 @@ class Tool
                 //在这里将图文消息链接地址插入到采集队列库中（队列库将在后文介绍，主要目的是建立一个批量采集队列，另一个程序将根据队列安排下一个采集的公众号或者文章内容）
                 //在这里根据$content_url从数据库中判断一下是否重复
 
-                $state = $this->checkArticle($v['app_msg_ext_info']['content_url']);
+                $state = $this->checkArticle($v['app_msg_ext_info']['content_url'], $v['app_msg_ext_info']['title'], $user_id);
                 if ($state === false) {
                     echo "跳过 {$v['app_msg_ext_info']['title']} \n";
                     continue;
@@ -57,7 +61,7 @@ class Tool
                 $content_url = $state;
 
                 // 存入文章标题表
-                $add[] = [
+                /* $add[] = [
                     'biz' => $biz,
                     'field_id' => $v['app_msg_ext_info']['fileid'], //一个微信给的id
                     'title' => $v['app_msg_ext_info']['title'], //文章标题
@@ -69,13 +73,24 @@ class Tool
                     'is_multi' => $is_multi,
                     'is_top' => 1,
                     'datetime' => time(),
+                ]; */
+
+                $add[] = [
+                    'user_id' => $user_id,
+                    'title' => $v['app_msg_ext_info']['title'], //文章标题
+                    'digest' => $v['app_msg_ext_info']['digest'], //文章摘要
+                    'article_img' => str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['cover'])), // 文章列表图
+                    'source_url' => $content_url,
+                    'content' => '',
+                    'weixin_article_id' => $v['app_msg_ext_info']['fileid'], //一个微信给的id
+                    'datetime' => $v['comm_msg_info']['datetime'],
                 ];
 
                 //如果是多图文消息
                 if ($is_multi == 1) {
                     foreach ($v['app_msg_ext_info']['multi_app_msg_item_list'] as $kk => $vv) {
 
-                        $state = $this->checkArticle($vv['content_url']);
+                        $state = $this->checkArticle($vv['content_url'], $vv['title'], $user_id);
                         if ($state === false) {
                             echo "跳过 {$vv['title']} \n";
                             continue;
@@ -83,7 +98,7 @@ class Tool
                         $content_url = $state;
 
                         //现在存入数据库
-                        $add[] = [
+                        /* $add[] = [
                             'biz' => $biz,
                             'field_id' => $vv['fileid'], //一个微信给的id
                             'title' => $vv['title'], //文章标题
@@ -95,7 +110,20 @@ class Tool
                             'is_multi' => 1,
                             'is_top' => 0,
                             'datetime' => time(),
+                        ]; */
+
+
+                        $add[] = [
+                            'user_id' => $user_id,
+                            'title' => $vv['title'],
+                            'digest' => htmlspecialchars($vv['digest']), //文章摘要
+                            'article_img' => str_replace("\\", "", htmlspecialchars_decode($vv['cover'])), //封面图片
+                            'source_url' => $content_url,
+                            'content' => '',
+                            'weixin_article_id' => $vv['fileid'],
+                            'datetime' => time(),
                         ];
+
                     }
                 }
             }
@@ -145,25 +173,44 @@ class Tool
     }
 
     //从数据库中查询biz是否已经存在，如果不存在则插入，这代表着我们新添加了一个采集目标公众号。
-    protected function checkBiz($biz)
+    protected function checkBiz($biz, $nickname, $logo)
     {
         // 公众号
-        $sql = "SELECT COUNT(*) FROM weixin WHERE biz='{$biz}'";
-        $biz_num = $this->db->getField($sql);
-        if ($biz_num == 0) {
-            $state = $this->db->insert('weixin', ['biz' => $biz]);
-            if (!$state) {
+        $sql = "SELECT id,biz,weixin_name,logo FROM users WHERE biz='{$biz}' OR weixin_name = '{$nickname}'";
+        $user_info = $this->db->find($sql);
+        if ($user_info) {
+            if (empty($user_info['biz']) || empty($user_info['weixin_name']) || empty($user_info['logo'])) {
+                $state = $this->db->update('users', ['biz' => $biz, 'weixin_name' => $nickname, 'logo' => $logo], "id = '{$user_info['id']}'");
+                if (!$state) {
+                    echo $this->db->getSql();
+                    echo "更新 biz {$biz} 失败\n";
+                }
+            }
+
+            $user_id = $user_info['id'];
+        } else {
+            $add = [
+                'user_name' => $nickname,
+                'logo' => $logo,
+                'weixin_name' => $nickname,
+                'biz' => $biz,
+                'state' => 0
+            ];
+            $user_id = $this->db->insert('users', $add);
+            if ($user_id < 1) {
                 echo "add weixin biz {$biz} error\n";
             }
         }
+
+        return $user_id;
     }
 
     // 检查文章是否抓取过。没有就添加到抓取队列中
-    public function checkArticle($content_url)
+    public function checkArticle($content_url, $title, $user_id)
     {
         $content_url = $this->url_decode($content_url);
 
-        $sql = "select count(*) from post where content_url='{$content_url}'";
+        $sql = "SELECT COUNT(*) FROM article WHERE title='{$content_url}' AND user_id='{$user_id}'";
 
         $num = $this->db->getField($sql);
 
@@ -177,12 +224,28 @@ class Tool
     }
 
     // 添加文章信息
-    protected function addArticleIndex($data)
+    /* protected function addArticleIndex($data)
     {
         foreach ($data as $add) {
             $title = $add['title'];
 
             $lastId = $this->db->insert('post', $add);
+            if ($lastId) {
+                echo "头条标题：" . $title . $lastId . "\n"; //这个echo可以显示在anyproxy的终端里
+            } else {
+                $error_log = "入库失败2：" . json_encode($add, JSON_UNESCAPED_UNICODE) . "\n";
+                file_put_contents('error.log', $error_log, FILE_APPEND | LOCK_EX);
+                echo "入库失败：" . $title . "\n"; //这个echo可以显示在anyproxy的终端里
+            }
+        }
+    } */
+
+    protected function addArticleIndex($data)
+    {
+        foreach ($data as $add) {
+            $title = $add['title'];
+
+            $lastId = $this->db->insert('article', $add);
             if ($lastId) {
                 echo "头条标题：" . $title . $lastId . "\n"; //这个echo可以显示在anyproxy的终端里
             } else {
--- server/test.js
+++ server/test.js
@@ -10,12 +10,22 @@ module.exports = {
       try {//防止报错退出程序
         var reg = /msgList = (.*?);/;//定义历史消息正则匹配规则
         var ret = reg.exec(newResponse.body.toString());//转换变量为string
-        HttpPost(ret[1],requestDetail.url,"/index.php?op=getMsgJson");//这个函数是后文定义的，将匹配到的历史消息json发送到自己的服务器
+
+        var data = {
+          str: ret[1],
+          url: requestDetail.url
+        };
+
+        HttpPost(data, "/index.php?op=getMsgJson");//这个函数是后文定义的，将匹配到的历史消息json发送到自己的服务器
       } catch(e) {//如果上面的正则没有匹配到，那么这个页面内容可能是公众号历史消息页面向下翻动的第二页，因为历史消息第一页是html格式的，第二页就是json格式的。
         try {
           var json = JSON.parse(newResponse.body.toString());
           if (json.general_msg_list != []) {
-            HttpPost(json.general_msg_list,requestDetail.url,"/index.php?op=getMsgJson");//这个函数和上面的一样是后文定义的，将第二页历史消息的json发送到自己的服务器
+            var data = {
+              str: json.general_msg_list,
+              url: requestDetail.url
+            };
+            HttpPost(data, "/index.php?op=getMsgJson");//这个函数和上面的一样是后文定义的，将第二页历史消息的json发送到自己的服务器
           }
         }catch(e){
           console.log(e);//错误捕捉
@@ -25,7 +35,24 @@ module.exports = {
       try {
         var reg = /var msgList = \'(.*?)\';/;//定义历史消息正则匹配规则（和第一种页面形式的正则不同）
         var ret = reg.exec(newResponse.body.toString());//转换变量为string
-        HttpPost(ret[1],requestDetail.url,"/index.php?op=getMsgJson");//这个函数是后文定义的，将匹配到的历史消息json发送到自己的服务器
+
+        var nickname = /var nickname = \"(.*?)\"/.exec(newResponse.body.toString());
+        var biz = /var __biz = \"(.*?)\";/.exec(newResponse.body.toString());
+
+        var xpath = require('xpath'),
+          dom = require('xmldom').DOMParser;
+
+        var doc = new dom().parseFromString(newResponse.body.toString());
+        var nodes = xpath.select('//img[@id="icon"]/@src', doc);
+
+        var data = {
+          str: ret[1],
+          nickname: nickname[1],
+          logo: nodes[0].value,
+          url: requestDetail.url
+        };
+
+        HttpPost(data, "/index.php?op=getMsgJson");//这个函数是后文定义的，将匹配到的历史消息json发送到自己的服务器
       }catch(e){
         console.log(e);//错误捕捉
       }
@@ -33,7 +60,11 @@ module.exports = {
       try {
         var json = JSON.parse(newResponse.body.toString());
         if (json.general_msg_list != []) {
-          HttpPost(json.general_msg_list,requestDetail.url,"/index.php?op=getMsgJson");//这个函数和上面的一样是后文定义的，将第二页历史消息的json发送到自己的服务器
+          var data = {
+            str: json.general_msg_list,
+            url: requestDetail.url
+          };
+          HttpPost(data,"/index.php?op=getMsgJson");//这个函数和上面的一样是后文定义的，将第二页历史消息的json发送到自己的服务器
         }
       }catch(e){
         console.log(e);
@@ -44,12 +75,8 @@ module.exports = {
   }
 };
 
-function HttpPost(str, url, path) {//将json发送到服务器，str为json内容，url为历史消息页面地址，path是接收程序的路径和文件名
+function HttpPost(data, path) {//将json发送到服务器，str为json内容，url为历史消息页面地址，path是接收程序的路径和文件名
   var http = require('http');
-  var data = {
-    str: encodeURIComponent(str),
-    url: encodeURIComponent(url)
-  };
   content = require('querystring').stringify(data);
   var options = {
     method: "POST",
